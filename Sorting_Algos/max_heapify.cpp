b3a70839783bf6d4813bf03f39031cca2794e483
Einsteinia <informationviakate2004@gmail.com>
2023-02-15 19:09:11 +0530

    addups

--- Sorting_Algos/max_heapify.cpp
+++ Sorting_Algos/max_heapify.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 using namespace std;
 void max_heap(int *a, int m, int n) {
-   int j, t;
-   t = a[m];
-   j = 2 * m;
-   while (j <= n) {
-      if (j < n && a[j+1] > a[j])
-         j = j + 1;
-      if (t > a[j])
-         break;
-      else if (t <= a[j]) {
-         a[j / 2] = a[j];
-         j = 2 * j;
-      }
-   }
-   a[j/2] = t;
-   return;
+    int j, t;
+    t = a[m];
+    j = 2 * m;
+    while (j <= n) {
+        if (j < n && a[j+1] > a[j])
+            j = j + 1;
+        if (t > a[j])
+            break;
+        else if (t <= a[j]) {
+            a[j / 2] = a[j];
+            j = 2 * j;
+        }
+    }
+    a[j/2] = t;
+    return;
 }
 void build_maxheap(int *a,int n) {
    int k;
