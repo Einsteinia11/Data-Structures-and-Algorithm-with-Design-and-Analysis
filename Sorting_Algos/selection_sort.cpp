22a1382722bfa04b63120f16cf727734d2013a3b
Einsteinia <informationviakate2004@gmail.com>
2023-03-04 17:40:02 +0530

    addups

--- Sorting_Algos/selection_sort.cpp
+++ Sorting_Algos/selection_sort.cpp
@@ -1,19 +1,45 @@
 #include <iostream>
 using namespace std;
-const int n;
 class selection_sort{
     public:
-    int arr[n];
+    int n;
+    int arr[];
+    selection_sort(){
+        cout<<"Enter the value of n: "<<endl;
+        cin>>n;
+    }
+    void insert(){
+        for(int i = 0; i<n; i++){
+            cout<<"Enter the value at"<<i<<"th position"<<endl;
+            cin>>arr[i];
+        }
+    }
     void sort(){
-
+        int i, j, small;
+        for(int i= 0; i<n; i++){
+            small = i;
+            for(int j= i+1; j<n; j++){
+                if(arr[j]<arr[small]){
+                    small = j;
+                }
+            }
+            int temp = arr[small];
+            arr[small] = arr[i];
+            arr[i] = temp;
+        }
+        for(int i = 0; i<n; i++){
+            cout<<arr[i]<<", ";
+        }
+        cout<<endl;
     }
 };
 
 int main(){
     //This program is written by kajal
-    cout<<"Enter the value of n: "<<endl;
-    cin>>n;
+    
     selection_sort obj;
+    obj.insert();
     obj.sort();
+
     return 0;
 }
