c371bacdf586da72cceb531c9188806f2674b2ad
Einsteinia <informationviakate2004@gmail.com>
2023-05-06 15:38:22 +0530

    commit

--- College_Practicals/q1.cpp
+++ College_Practicals/q1.cpp
@@ -16,52 +16,52 @@ public :
 template<class T>
 void insort<T>::input(int size)
 {  
-for(int i=1;i<size;i++)
-    {
-        int ele=rand();
-        a[i]=ele;
-    }
+    for(int i=1;i<size;i++)
+        {
+            int ele=rand();
+            a[i]=ele;
+        }
 }
 template<class T>
 void insort<T>::output(int size)
 {
-cout<<"\n Sorted array : ";
-for(int i=1;i<size;i++)
-cout<<a[i]<<" ";
+    cout<<"\n Sorted array : ";
+    for(int i=1;i<size;i++)
+    cout<<a[i]<<" ";
 }
 template<class T>
 void insort<T>::isort(int size)
 {
     int key,count=0,i;
-ofstreamfout("sort.csv",ios::app);
-for(int j=2;j<size;j++)
+    ofstreamfout("sort.csv",ios::app);
+    for(int j=2;j<size;j++)
     {
         key=a[j];
-i=j-1;
+        i=j-1;
         while(i>0 && a[i]>key)
         {
             a[i+1]=a[i];
-i=i-1;
+            i=i-1;
             count++;
         }
         count++;
-    a[i+1]=key;
+        a[i+1]=key;
     }
-fout<<(size-1)<<" ,"<<count<<","<<log2(size-1)<<","<<(size-1)*log2(size-1)<<","<<(size-1)*(size-1)<<endl;
-fout.close();
+    fout<<(size-1)<<" ,"<<count<<","<<log2(size-1)<<","<<(size-1)*log2(size-1)<<","<<(size-1)*(size-1)<<endl;
+    fout.close();
 }
 int main()
 {  
     int size;
-cout<<"\n Enter size of array : ";
-cin>>size;
-for(int i=0;i<20;i++)
+    cout<<"\n Enter size of array : ";
+    cin>>size;
+    for(int i=0;i<20;i++)
     {
-insort<int>o1;
-      o1.input( size);
-      o1.isort(size);
-      o1.output(size);
-      size=size+20;
+        insort<int>o1;
+        o1.input( size);
+        o1.isort(size);
+        o1.output(size);
+        size=size+20;
     }
     return 0;
 }
