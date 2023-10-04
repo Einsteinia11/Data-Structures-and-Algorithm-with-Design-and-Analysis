018a822f384e1339fe55adc4fef752160af88fe1
Pranav Jain <95164530+pranavjainjs@users.noreply.github.com>
2023-10-04 19:43:43 +0530

    Update Sieve of Eratosthenes.cpp
    The previous code was formatted badly and did not have comments.

--- number_theory algorithms/Sieve of Eratosthenes.cpp
+++ number_theory algorithms/Sieve of Eratosthenes.cpp
@@ -1,23 +1,28 @@
-int countPrimes(int n)
-{
-int p[n+1]={0};
-int c=0;
-for(int i=2;i*i<=n;i++)//O(nlog(log(n)))
-{
-if(p[i]==0)
-{
-for(int j=2*i;j<=n;j+=i)
-{
-p[j]=1;
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to check if a number is prime using the Sieve of Eratosthenes algorithm
+int sieve(int n) {  // O(nloglogn)
+    vector<bool> is_prime(n + 1, true); 
+    is_prime[0] = is_prime[1] = false; 
+    for (int i = 2; i * i <= n; i++) {
+        if (is_prime[i]) {
+            for (int j = i * i; j <= n; j += i) 
+                is_prime[j] = false;
+        }
+    }
+    return is_prime[n]; // Returns true if n is prime, false otherwise
 }
-}
-}
-for(int i=2;i<n;i++)
-{
-if(p[i]==0)
-{
-c++;
-}
-}
-return c;
+
+int main() {
+    int n;
+    cout << "Enter a number. I will tell if it is prime or not." << endl;
+    cin >> n;
+
+    // Check if the input number is prime using the sieve function
+    if (sieve(n)) {
+        cout << n << " is prime\n"; // Output if the number is prime
+    } else {
+        cout << n << " is not prime\n"; // Output if the number is not prime
+    }
 }
