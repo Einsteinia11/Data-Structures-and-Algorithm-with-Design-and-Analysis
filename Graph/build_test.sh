8cd7f9cf2470644317211682a42c7364a93ef775
blaze <prabhatkumarverma42@gmail.com>
2023-10-04 23:12:14 +0530

    build script

--- Graph/build_test.sh
+++ Graph/build_test.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Requirements -
+#               1. cmake for building
+#               2. google test for testing
+#                     - can use online - uncomment the marked lines on tests/CMakeLists.txt
 # Create a 'build' directory if it doesn't exist
 mkdir -p ./build
 
