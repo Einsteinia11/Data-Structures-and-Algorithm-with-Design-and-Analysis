772794eeaca813d52236ccaef941d1094958bc82
blaze <prabhatkumarverma42@gmail.com>
2023-10-04 23:07:05 +0530

    script for building and testing Implementations

--- Graph/build_test.sh
+++ Graph/build_test.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-mkdir ./build
-cd build
+# Create a 'build' directory if it doesn't exist
+mkdir -p ./build
+
+# Change to the 'build' directory
+cd ./build
+
+# Configure the project with CMake
 cmake ..
+
+# Build the project using 'make'
 make
+
+# Run tests using 'ctest'
 ctest
+
