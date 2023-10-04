e892eb14047b1e5120a41f2f470ac3d5db2a4c03
blaze <prabhatkumarverma42@gmail.com>
2023-10-04 22:48:06 +0530

    changed from vector to unoordered map style graph, added extra functionlities

--- Graph/Header/Graph/WeightedGraph.h
+++ Graph/Header/Graph/WeightedGraph.h
@@ -1,17 +1,17 @@
 #ifndef WEIGHTEDGRAPH_H
 #define WEIGHTEDGRAPH_H
 
+#include <unordered_map>
 #include <vector>
 
 class WeightedGraph {
 private:
-    int V; // Number of vertices
     const bool isWeighted = true; // Immutable flag to indicate if the graph is weighted
-    std::vector<std::vector<std::pair<int, int>>> adjList; // Adjacency list with weights
+    std::unordered_map<int, std::vector<std::pair<int, int>>> adjList; // Adjacency list with weights
 
 public:
-    // Constructor to create a weighted graph with 'vertices' number of vertices
-    WeightedGraph(int vertices);
+    // Constructor to create a weighted graph
+    WeightedGraph();
 
     // Function to add a unidirectional edge from 'src' to 'dest' with a given 'weight'
     bool addUniDirectionalEdge(int src, int dest, int weight);
@@ -28,13 +28,31 @@ public:
     // Function to print the adjacency list representation of the graph
     void printGraph() const;
 
+    // Function to add a vertex to the graph
+    bool addVertex(int vertex);
+
+    // Function to remove a vertex from the graph
+    bool removeVertex(int vertex);
+
+    // Function to get the number of vertices in the graph
+    int getVertexCount() const;
+
+    // Function to get the set of vertices in the graph
+    std::vector<int> getVertices() const;
+
     // Remove a unidirectional edge from vertex u to v
     bool removeEdge(int src, int dest);
 
     // Remove a bidirectional edge between vertices u and v
     bool removeBiDirectionalEdge(int src, int dest);
-   
-  // Other member functions...
+    
+    // Function to check if a vertex exists in the graph
+    bool hasVertex(int vertex);
+
+    // Function to get the weight of the edge between 'src' and 'dest'
+    int getEdgeWeight(int src, int dest);
+
+    // Other member functions...
 };
 
 #endif
