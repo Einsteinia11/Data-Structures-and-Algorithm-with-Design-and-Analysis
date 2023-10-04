5c7cf0df859554b9bd2a711f548c720143c9484f
Anurag Srivastav <anuragsrivastav0027@gmail.com>
2023-10-04 23:55:13 +0530

    added comments to code

--- Sorting_Algos/count_sort.cpp
+++ Sorting_Algos/count_sort.cpp
@@ -1,28 +1,41 @@
 #include<bits/stdc++.h>
-using namespace std;
+using namespace std; 
+
 void countSort(int arr[],int n)
 {
-    int max=INT_MIN,min=INT_MAX;
+    // Write your code here.
+    int max=arr[0];
+    int min=arr[0];
+    // to solve problem of negative number we will use concept of correction factor
+    // correction factor=abs(min) so that -5->0, -4->1,
     for(int i=1;i<n;i++)
     {
-        if(arr[i]>max) max=arr[i];
-        if(arr[i]<min) min=arr[i];
+        if(arr[i]>max)
+           max=arr[i];
+        if(arr[i]<min)
+           min=arr[i];   
     }
     int correctionfactor=abs(min);
+    // adding correction factor to elements of array to make array positive
     for(int i=0;i<n;i++)
     {
         arr[i]+=correctionfactor;
     }
+    // now array to store count will be of length max+countfactor+1
     vector<int> count(max+correctionfactor+1,0);
     for(int i=0;i<n;i++)
     {
         count[arr[i]]++;
     }
+    // count now stores the corect position of element in ans array 
     for(int i=1;i<count.size();i++)
     {
         count[i]+=count[i-1];
     }
     int ans[n];
+    // in loop we are taking element from array and finding its correct location
+    // from count array[count-1 is correct location] and decreasing count by 1
+    // decreasing countfactor from arr[i] so that array becomes normal again
     for(int i=n-1;i>=0;i--)
     {
         ans[count[arr[i]]-1]=arr[i]-correctionfactor;
--- Sorting_Algos/radiax_sort.cpp
+++ Sorting_Algos/radiax_sort.cpp
@@ -1,7 +1,12 @@
+// Radix Sort is a linear sorting algorithm that sorts elements by processing them digit by digit. 
+// It is an efficient sorting algorithm for integers or strings with fixed-size keys. 
+
 #include<bits/stdc++.h>
 using namespace std;
 int nth(int value,int digit)
 {
+    // nth function return nth digit in a digit
+    // if no=121772 ans digit= 5 it will return 2
     string val=to_string(value);
     if(val.size()<digit)
         return 0;
@@ -12,12 +17,16 @@ int nth(int value,int digit)
 }
 void radixSort(int arr[],int n)
 {
+    // creating 9 queues to store numbers based on their digits 
     vector<queue<int>> queues(10);
     int maxi=0;
+    // calculating the max number ina n array and calculating number of digits in it
     for(int i=0;i<n;i++)
         if(arr[i]>maxi)
             maxi=arr[i];
     int maxDigit= to_string(maxi).size();
+
+    // pushing number into their corresponding buckets based on digits
     for(int i=0;i<maxDigit;i++)
     {
         for(int j=0;j<n;j++)
@@ -26,6 +35,7 @@ void radixSort(int arr[],int n)
         }
         int p=0;
         for(int j=0;j<10;j++){
+                // popping element from queue to get their correct position after nth operation
                 while(!queues[j].empty())
                 {
                     arr[p]=queues[j].front();
