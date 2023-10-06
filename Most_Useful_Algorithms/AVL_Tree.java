0ab6485d936b8ea3b4284535dc2c9474afa7f814
Akshat Mishra <106366272+akshatmishra25@users.noreply.github.com>
2023-10-06 17:53:34 +0530

    Update AVL_Tree.java with proper comments

--- Most_Useful_Algorithms/AVL_Tree.java
+++ Most_Useful_Algorithms/AVL_Tree.java
@@ -1,85 +1,93 @@
 public class AvlTree {
-    public class Node{
+    // Inner class representing a node in the AVL tree
+    public class Node {
         private int value;
         private int height;
         private Node left;
         private Node right;
 
-        public Node(int value){
-           this.value = value;
+        public Node(int value) {
+            this.value = value;
         }
 
-        public int getValue(){
+        public int getValue() {
             return value;
         }
     }
 
     private Node root;
 
-    public AvlTree(){
-
+    public AvlTree() {
+        // Constructor for AVL Tree, initially, it's empty.
     }
-    public int height(){
+
+    // Get the height of the tree
+    public int height() {
         return height(root);
     }
-    private int height(Node node){
-        if(node==null){
+
+    // Get the height of a specific node
+    private int height(Node node) {
+        if (node == null) {
             return -1;
         }
         return node.height;
     }
 
+    // Check if the tree is empty
     public boolean isEmpty() {
         return root == null;
     }
 
-    public void insert(int value){
-        root=insert(value, root);
+    // Insert a value into the AVL tree
+    public void insert(int value) {
+        root = insert(value, root);
     }
 
-    private Node insert(int value, Node node){
-        if(node==null){
+    // Helper method for inserting a value into the AVL tree
+    private Node insert(int value, Node node) {
+        if (node == null) {
             node = new Node(value);
             return node;
         }
 
-        if(value<node.value){
-            node.left=insert(value, node.left);
+        if (value < node.value) {
+            node.left = insert(value, node.left);
+        } else if (value > node.value) {
+            node.right = insert(value, node.right);
         }
 
-        else if(value>node.value){
-            node.right=insert(value, node.right);
-        }
+        // Update the height of the current node
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
 
-        node.height = Math.max(height(node.left), height(node.right))+1;
-
+        // Perform rotation if necessary to maintain balance
         return rotate(node);
     }
-    private Node rotate(Node node){
-        if(height(node.left)-height(node.right)>1){
-            // left heavy
-            if(height(node.left.left)-height(node.left.right)>0){
-                // left left case
+
+    // Perform rotations to maintain balance in the AVL tree
+    private Node rotate(Node node) {
+        if (height(node.left) - height(node.right) > 1) {
+            // Left-heavy
+            if (height(node.left.left) - height(node.left.right) > 0) {
+                // Left-left case
                 return rightRotate(node);
             }
-
-            if(height(node.left.left)-height(node.left.right)<0){
-                // left right case
+            if (height(node.left.left) - height(node.left.right) < 0) {
+                // Left-right case
                 node.left = leftRotate(node.left);
                 return rightRotate(node);
             }
         }
 
-        if(height(node.left)-height(node.right)<-1){
-            // right heavy
-            if(height(node.right.left)-height(node.right.right)<0){
-                // right right case
+        if (height(node.left) - height(node.right) < -1) {
+            // Right-heavy
+            if (height(node.right.left) - height(node.right.right) < 0) {
+                // Right-right case
                 return leftRotate(node);
             }
-
-            if(height(node.right.left)-height(node.right.right)>0){
-                // left right case
-                node.right = rightRotate(node.left);
+            if (height(node.right.left) - height(node.right.right) > 0) {
+                // Right-left case
+                node.right = rightRotate(node.right);
                 return leftRotate(node);
             }
         }
@@ -87,75 +95,85 @@ public class AvlTree {
         return node;
     }
 
-    public Node rightRotate(Node p){
+    // Perform a right rotation
+    public Node rightRotate(Node p) {
         Node c = p.left;
         Node t = c.right;
-        c.right=p;
-        p.left=t;
-        p.height = Math.max(height(p.left), height(p.right)+1);
-        c.height = Math.max(height(c.left), height(c.right)+1);
-
+        c.right = p;
+        p.left = t;
+        p.height = Math.max(height(p.left), height(p.right) + 1);
+        c.height = Math.max(height(c.left), height(c.right) + 1);
         return c;
     }
 
-    public Node leftRotate(Node c){
+    // Perform a left rotation
+    public Node leftRotate(Node c) {
         Node p = c.right;
         Node t = p.left;
-        p.left=c;
-        c.right=t;
-        p.height = Math.max(height(p.left), height(p.right)+1);
-        c.height = Math.max(height(c.left), height(c.right)+1);
-
+        p.left = c;
+        c.right = t;
+        p.height = Math.max(height(p.left), height(p.right) + 1);
+        c.height = Math.max(height(c.left), height(c.right) + 1);
         return p;
     }
 
-    public void populate(int[] nums){
-        for(int i=0; i<nums.length; i++){
+    // Populate the AVL tree with an array of values
+    public void populate(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
             this.insert(nums[i]);
         }
     }
 
-    public void populateSorted(int[] nums){
+    // Populate the AVL tree with a sorted array of values
+    public void populateSorted(int[] nums) {
         populateSorted(nums, 0, nums.length);
     }
-    private void populateSorted(int[] nums, int start, int end){
-        if(start>=end){
+
+    // Helper method to populate the AVL tree with a sorted array of values
+    private void populateSorted(int[] nums, int start, int end) {
+        if (start >= end) {
             return;
         }
-        int mid = (start+end)/2;
-
+        int mid = (start + end) / 2;
         this.insert(nums[mid]);
         populateSorted(nums, start, mid);
-        populateSorted(nums, mid+1, end);
+        populateSorted(nums, mid + 1, end);
     }
 
-    public boolean balanced(){
+    // Check if the AVL tree is balanced
+    public boolean balanced() {
         return balanced(root);
     }
-    private boolean balanced(Node node){
-        if(node==null){
+
+    // Helper method to check if a specific node and its subtrees are balanced
+    private boolean balanced(Node node) {
+        if (node == null) {
             return true;
         }
-        return Math.abs(height(node.left)-height(node.right))<=1 && balanced(node.left) && balanced(node.right);
+        return Math.abs(height(node.left) - height(node.right)) <= 1 && balanced(node.left) && balanced(node.right);
     }
 
-    public void display(){
+    // Display the AVL tree
+    public void display() {
         display(this.root, "Root Node: ");
     }
-    private void display(Node node, String details){
-        if(node == null){
+
+    // Helper method to display the AVL tree
+    private void display(Node node, String details) {
+        if (node == null) {
             return;
         }
-        System.out.println(details+node.value);
-        display(node.left, "Left child of :"+node.value+" : ");
-        display(node.right, "Right child of :"+node.value+" : ");
+        System.out.println(details + node.value);
+        display(node.left, "Left child of :" + node.value + " : ");
+        display(node.right, "Right child of :" + node.value + " : ");
     }
 
+    // Main method for testing
     public static void main(String[] args) {
         AvlTree tree = new AvlTree();
-        for(int i=0; i<10000; i++){
+        for (int i = 0; i < 10000; i++) {
             tree.insert(i);
-        }   
+        }
         System.out.println(tree.height());
     }
 }
